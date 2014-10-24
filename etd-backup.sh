119e4fdb2a9b5c05c9859b2a609325a245ef0ec1
jbanety <jbanety@gmail.com>
2014-10-24 14:42:47 +0200

    correction date + fomatage

--- etd-backup.sh
+++ etd-backup.sh
@@ -13,56 +13,82 @@ set -eu
 start=$(date +"%s")
 
 ## Paramètres
+
 # @var string Nom de la sauvegarde
 SAUVNAME=`hostname`
+
 # @var string Répertoire de stockage des sauvegardes
 DATADIR="/var/etd-backup"
+
 # @var string Répertoire de travail (création/compression)
 DATATMP="/var/tmp/etd-backup"
+
 # @var string Nom du dossier du dump
 DATANAME="dump_$(date +%d.%m.%y@%Hh%M)"
+
 # @var string Commande pour la compression
 COMPRESSION_CMD="tar -cf"
+
 # @var string Options à passer pour la commande de compression
 COMPRESSION_OPT="--use-compress-prog=pbzip2"
+
 # @var string Extension des archives
 COMPRESSION_EXT=".tar.bz2"
+
 # @var string Fréquence des sauvegardes complètes
 FULLIFOLDERTHAN="1W"
+
 # @var integer Le nombre de jours de rétention des sauvegardes
 RETENTION="60"
+
 # @var string Utilisateur MySQL utilisé pour le dump
 DB_USER="etdbackup"
+
 # @var string Mot de passe
 DB_PASS="@cvyizqB9qNL8zi@"
+
 # @var string Noms des bases de données à exclure (regex)
 DB_EXCLUSIONS="(information_schema|performance_schema)"
+
 # @var string Chemin vers le dossier des sites internet
 WWW_ROOTDIR="/var/www/vhosts"
+
 # @var string Noms des dossiers à exclure
 WWW_EXCLUSIONS="(system|chroot|default|fs|fs-passwd)"
+
 # Utilisateur Hubic
 HUBIC_USER="contact@etudoo.fr"
+
 # Mot de passe HubiC
 HUBIC_PASSWORD="@AxF9hsCoQTktyY@"
+
 # Application client id Hubic
 HUBIC_APPID="api_hubic_BcWEnGVgNIOqj7zkNBS4QEqTy7Z3tHNl"
+
 # Application client secret Hubic
 HUBIC_APPSECRET="FBGPZutXbIN2kkFG240kseO6XeWrmemBm29Iep2eQM1NXlspe4ChH0YielJdZ85d"
+
 # Application domaine de redirection Hubic
 HUBIC_APPURLREDIRECT="http://localhost/"
+
 # Dossier Hubic dans lequel seront envoyées les sauvegardes
 HUBIC_FOLDER="cazin_backups"
+
 # Passphrase pour le chiffrement des sauvegardes
 PASSPHRASE="@BjH4zzY4bV6L5U@"
+
 # @var string Email pour les erreurs (0 pour désactiver)
 EMAIL="support@etd-solutions.com"
+
 # @var string Chemin vers l'exécutable duplicity
 DUPLICITY_BIN="/usr/bin/duplicity"
+
 # @var string Ficher de log d'erreur
 LOGERROR="/var/log/etd-backup.log"
+
 # @var bool Envoi un rapport par email sur l'état des backups
 RAPPORT=1
+
 
 # Log d'erreur
 exec 2> ${LOGERROR}
@@ -154,7 +180,7 @@ unset CLOUDFILES_APIKEY
 unset PASSPHRASE
 
 end=$(date +"%s")
-diff=$(($date2-$date1))
+diff=$(($end-$start))
 
 echo "Sauvegarde terminée à $end en $(($diff / 60)) minutes et $(($diff % 60)) secondes."
 exit 0
